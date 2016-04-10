277fc56c09ff246581b80140a6c57b1fd7b7b16a
Max Fechner <max.fechner@gmail.com>
2016-04-10 14:21:51 +0200

    ...

--- index.js
+++ index.js
@@ -8423,23 +8423,95 @@ Polymer({
         }
 
     });
+MegaCities = [{
+		"city": "Shanghai",
+		"country": "China",
+		"population": 36,
+		"lat": 31.20,
+		"lon": 121.50
+	}, {
+		"city": "New York",
+		"country": "United States",
+		"population": 24,
+		"lat": 40.71,
+		"lon": -74.00
+	}, {
+		"city": "Bangkok",
+		"country": "Thailand",
+		"population": 15,
+		"lat": 13.76,
+		"lon": 100.50
+	}, {
+		"city": "Tokyo",
+		"country": "Japan",
+		"population": 39,
+		"lat": 35.68,
+		"lon": 139.68
+	}, {
+		"city": "Jakarta",
+		"country": "Indonesia",
+		"population": 32,
+		"lat": -6.17,
+		"lon": 106.82
+	}, {
+		"city": "Seoul",
+		"country": "South Korea",
+		"population": 26,
+		"lat": 37.57,
+		"lon": 126.96
+	}, {
+		"city": "Delhi",
+		"country": "India",
+		"population": 22,
+		"lat": 28.61,
+		"lon": 77.21
+	}, {
+		"city": "Sao Paulo",
+		"country": "Brazil",
+		"population": 22,
+		"lat": -23.55,
+		"lon": -46.63
+	}, {
+		"city": "Lagos",
+		"country": "Nigeria",
+		"population": 21,
+		"lat": 6.52,
+		"lon": 3.38
+	}, {
+		"city": "London",
+		"country": "England",
+		"population": 14,
+		"lat": 51.51,
+		"lon": -0.13
+	}, {
+		"city": "Paris",
+		"country": "France",
+		"population": 12,
+		"lat": 48.86,
+		"lon": 2.35
+	}, {
+		"city": "Moscow",
+		"country": "Russia",
+		"population": 17,
+		"lat": 55.76,
+		"lon": 37.62
+	}, {
+		"city": "Cairo",
+		"country": "Egypt",
+		"population": 19,
+		"lat": 30.04,
+		"lon": 31.24
+	}
+
+
+
+];
 Polymer({
         is: "mega-cities",
 
         ready: function () {
-            STORE.get('MegaCities',function(cities){
-                this.set('defaults',cities);
-            }.bind(this));
-            STORE.onChange('MegaCities',function(key,cities){
-                console.log("STORE-change",key,cities);
-                this.set('defaults',cities);
-            }.bind(this));
-            GEO.ajax('http://api.max.pub/MegaCities.json', function(cities){
-                cities = JSON.parse(cities);
-                cities.sort(this.populationSort);
-                STORE.set('MegaCities',cities);
-                // this.set('defaults',cities);
-            }.bind(this));
+            MegaCities.sort(this.populationSort);
+            this.set('defaults',MegaCities);
         },
         
         citySort: function(a,b){ // asc
@@ -8801,5 +8873,5 @@ Polymer({
     });
 
     setLocation = function(lat,lon){
-        document.location.hash = '#'+ (lat*1).toFixed(2)+'#'+(lon*1).toFixed(2);
+        document.location.hash = ''+ (lat*1).toFixed(2)+','+(lon*1).toFixed(2);
     };
