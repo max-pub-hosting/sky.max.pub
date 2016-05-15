6952f9a7cc1df69464ce18743783f26396058f57
Max Fechner <max.fechner@gmail.com>
2016-05-15 10:24:59 +0200

    ...

--- index.js
+++ index.js
@@ -8984,21 +8984,6 @@ GEO.provider.search.google = { // using google maps api... implementing bing,OSM
 };
 
 // console.log("LOADING GOOGLE", GEO.provider.search);
-if (!GEO) GEO = {};
-
-
-GEO.provider.ip.ipinfo_io = function(callback) {
-	GEO.ajax('http://ipinfo.io/json/', function(data) {
-		data = JSON.parse(data);
-		GEO.log('ipinfo ' + data);
-		var latlon = data.loc.split(',');
-		GEO.location.ip.provider = 'ipinfo.io';
-		GEO.location.ip.lat = latlon[0];
-		GEO.location.ip.lon = latlon[1];
-		GEO.location.ip.raw = data;
-		if (callback) callback(GEO.location.ip);
-	});
-};
 Polymer({
         is: "app-root",
         setHash: function(ev){
